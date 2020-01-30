75bd672d8502e78832468c6ae4c99b7e65a0296a
v-sijagt <60467132+v-sijagt@users.noreply.github.com>
2020-01-30 19:49:04 +0530

    Update add-favorites.tsx

--- Source/Microsoft.Teams.Apps.BookAThing/ClientApp/src/components/add-favorites.tsx
+++ Source/Microsoft.Teams.Apps.BookAThing/ClientApp/src/components/add-favorites.tsx
@@ -575,8 +575,7 @@ class AddFavorites extends React.Component<IAddFavoriteProps, IState>
                 return response;
             }
             else {
-                this.appInsights.trackTrace({ message: `'TopFiveRoomsAsync' - Request failed:${searchedRooms.status}`, severityLevel: SeverityLevel.Warning });
-                this.setMessage(this.state.resourceStrings.ExceptionResponse, Constants.ErrorMessageRedColor, false);
+                this.appInsights.trackTrace({ message: `'SearchRoomAsync' - Request failed:${searchedRooms.status}`, severityLevel: SeverityLevel.Warning });
             }
         }
         else {
